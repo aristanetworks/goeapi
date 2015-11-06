3c5996863844fea6cf86daeec8628f6fd5a66706
Cheyne Womble <cwomble@arista.com>
2015-11-06 11:50:15 -0500

    Added Examples

---
+++ examples/example1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aristanetworks/goeapi"
+	"github.com/aristanetworks/goeapi/module"
+)
+
+func main() {
+	node, err := goeapi.ConnectTo("dut")
+	if err != nil {
+		panic(err)
+	}
+	conf := node.RunningConfig()
+	fmt.Println(conf)
+
+	var showversion module.ShowVersion
+	handle, _ := node.GetHandle("json")
+	handle.Enable(&showversion)
+	fmt.Println("\nVersion:", showversion.Version)
+
+	s := module.Show(node)
+	showData := s.ShowVersion()
+	fmt.Printf("\nModelname         : %s\n", showData.ModelName)
+	fmt.Printf("Internal Version  : %s\n", showData.InternalVersion)
+	fmt.Printf("System MAC        : %s\n", showData.SystemMacAddress)
+	fmt.Printf("Serial Number     : %s\n", showData.SerialNumber)
+	fmt.Printf("Mem Total         : %d\n", showData.MemTotal)
+	fmt.Printf("Bootup Timestamp  : %.2f\n", showData.BootupTimestamp)
+	fmt.Printf("Mem Free          : %d\n", showData.MemFree)
+	fmt.Printf("Version           : %s\n", showData.Version)
+	fmt.Printf("Architecture      : %s\n", showData.Architecture)
+	fmt.Printf("Internal Build ID : %s\n", showData.InternalBuildID)
+	fmt.Printf("Hardware Revision : %s\n", showData.HardwareRevision)
+
+	sys := module.System(node)
+	if ok := sys.SetHostname("Ladie"); !ok {
+		fmt.Printf("SetHostname Failed\n")
+	}
+	sysInfo := sys.Get()
+	fmt.Printf("\nSysinfo: %#v\n", sysInfo.HostName())
+}
---
+++ examples/example2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aristanetworks/goeapi"
+)
+
+type MyShowVlan struct {
+	SourceDetail string          `json:"sourceDetail"`
+	Vlans        map[string]Vlan `json:"vlans"`
+}
+
+type Vlan struct {
+	Status     string               `json:"status"`
+	Name       string               `json:"name"`
+	Interfaces map[string]Interface `json:"interfaces"`
+	Dynamic    bool                 `json:"dynamic"`
+}
+
+type Interface struct {
+	Annotation      string `json:"annotation"`
+	PrivatePromoted bool   `json:"privatePromoted"`
+}
+
+func (s *MyShowVlan) GetCmd() string {
+	return "show vlan configured-ports"
+}
+
+func main() {
+	node, err := goeapi.ConnectTo("dut")
+	if err != nil {
+		panic(err)
+	}
+
+	sv := &MyShowVlan{}
+
+	handle, _ := node.GetHandle("json")
+	handle.AddCommand(sv)
+	if err := handle.Call(); err != nil {
+		panic(err)
+	}
+
+	for k, v := range sv.Vlans {
+		fmt.Printf("Vlan:%s\n", k)
+		fmt.Printf("  Name  : %s\n", v.Name)
+		fmt.Printf("  Status: %s\n", v.Status)
+	}
+}
