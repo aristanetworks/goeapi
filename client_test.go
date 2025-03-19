4b37808ca9be84bbc7072d84e0a4b61ac774ed8e
roopeshsn <roopeshsaravanan.dev@gmail.com>
2025-03-19 17:01:40 +0530

    feat: add test suite for TLS

--- client_test.go
+++ client_test.go
@@ -1,4 +1,3 @@
-//
 // Copyright (c) 2015-2016, Arista Networks, Inc.
 // All rights reserved.
 //
@@ -6,16 +5,16 @@
 // modification, are permitted provided that the following conditions are
 // met:
 //
-//   * Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the following disclaimer.
+//   - Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
 //
-//   * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
 //
-//   * Neither the name of Arista Networks nor the names of its
-//   contributors may be used to endorse or promote products derived from
-//   this software without specific prior written permission.
+//   - Neither the name of Arista Networks nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -28,7 +27,6 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
 package goeapi
 
 import (
@@ -489,6 +487,24 @@ func TestClientConnectDefaultPort_UnitTest(t *testing.T) {
 	for idx, tt := range tests {
 		// 1.1.1.2 is assumed to be an unreachable bogus address
 		_, err := Connect(tt.proto, "1.1.1.2", "admin", "admin", tt.port)
+		if err != nil {
+			t.Fatalf("Test[%d] %s. Error in connect: err:%#v", idx, tt.descr, err)
+		}
+	}
+}
+
+func TestClientConnectTLSDefaultPort_UnitTest(t *testing.T) {
+	tests := [...]struct {
+		proto string
+		port  int
+		descr string
+	}{
+		{"https_certs", 443, "Https port 443"},
+	}
+
+	for idx, tt := range tests {
+		// 1.1.1.2 is assumed to be an unreachable bogus address
+		_, err := ConnectTLS(tt.proto, "1.1.1.2", "admin", "admin", tt.port)
 		if err != nil {
 			t.Fatalf("Test[%d] %s. Error in connect: err:%#v", idx, tt.descr, err)
 		}
